b4b426adcd0f711c53da5b519d3bb38a32003a3a
Madalina-Buza <Madalina-Buza@users.noreply.github.com>
2017-01-13 00:37:53 +0200

    Add files via upload

--- Proiect.cpp
+++ Proiect.cpp
@@ -154,3 +154,100 @@ void adaugareCopil() {
 		cout << "Copilul nu se incadreaza in nici o grupa." << endl;
 	}
 }
+void adaugareCopil() {
+	copil a;
+	copil d[25] = { NULL };
+	int nrCopii = 0, lastId = 0;
+	//	int i=0;
+	//cout << "ID: "; cin >> a.Id;
+	cout << "Nume copil: ";  cin >> a.nume;
+	cout << "Prenume copil: "; cin >> a.prenume;
+	cout << "Varsta: "; cin >> a.varsta;
+	cout << "Adresa: "; cout << "Strada: "; cin >> a.b.strada; cout << "Nr. "; cin >> a.b.nr;
+	cout << "Prenume mama: "; cin >> a.prenume_mama;
+	cout << "Prenume tata: "; cin >> a.prenume_tata;
+	if ((a.varsta == 0) || (a.varsta == 1)) {
+		populeazaVector(d, "Grupa1.txt");
+		for (int n = 0; n < 25; n++) {
+			if (d[n].Id != 0) {
+				nrCopii = nrCopii + 1;
+				lastId = d[n].Id;
+			}
+			else {
+				break;
+			}
+		}
+		if (nrCopii < 25) {
+			a.Id = lastId + 1;
+			scriereInFisier(a, "Grupa1.txt");
+		}
+		else {
+			cout << "Nu mai sunt locuri libere. Copil neinscris. " << endl;
+		}
+	}
+	else if ((a.varsta == 2) || (a.varsta == 3)) {
+		populeazaVector(d, "Grupa2.txt");
+		for (int n = 0; n < 25; n++) {
+			if (d[n].Id != 0) {
+				nrCopii = nrCopii + 1;
+				lastId = d[n].Id;
+			}
+			else {
+				break;
+			}
+		}
+		if (nrCopii < 25) {
+			a.Id = lastId + 1;
+			scriereInFisier(a, "Grupa2.txt");
+		}
+		else {
+			cout << "Nu mai sunt locuri libere. ";
+		}
+	}
+	else if ((a.varsta == 4) || (a.varsta == 5) || (a.varsta == 6)) {
+		populeazaVector(d, "Grupa3.txt");
+		for (int n = 0; n < 25; n++) {
+			if (d[n].Id != 0) {
+				nrCopii = nrCopii + 1;
+				lastId = d[n].Id;
+			}
+			else {
+				break;
+			}
+		}
+		if (nrCopii < 25) {
+			a.Id = lastId + 1;
+			scriereInFisier(a, "Grupa3.txt");
+		}
+		else {
+			cout << "Nu mai sunt locuri libere. ";
+		}
+	}
+	else {
+		cout << "Copilul nu se incadreaza in nici o grupa." << endl;
+	}
+}
+string listareCopii() {
+	int s;
+	string numeFisier;
+	copil d[25] = { NULL };
+	cout << "Din ce grupa doriti sa vedeti copiii? " << endl << "1. Grupa 1: copii cu 0 si 1 an " << endl << "2. Grupa 2: copii cu 2 si 3 ani " << endl << "3. Grupa 3: copii cu 4, 5 si 6 ani " << endl;
+	cin >> s;
+	switch (s) {
+	case 1: populeazaVector(d, "Grupa1.txt");
+		numeFisier = "Grupa1.txt";
+		break;
+	case 2: populeazaVector(d, "Grupa2.txt");
+		numeFisier = "Grupa2.txt";
+		break;
+	case 3: populeazaVector(d, "Grupa3.txt");
+		numeFisier = "Grupa3.txt";
+		break;
+	default:cout << "EROARE. Grupa inexistenta. " << endl; numeFisier = "EROARE"; break;
+	}
+	for (int j = 0; j < 25; j++) {
+		if (d[j].Id == 0) break;
+		cout << j + 1 << "." << " " << "ID: " << d[j].Id << " " << "Nume: " << d[j].nume << " " << "Prenume: " << d[j].prenume << " " << "Varsta: " << d[j].varsta << " " << "Adresa: Strada: " << d[j].b.strada << " " << "Nr. " << d[j].b.nr << " " << "Prenume mama: " << d[j].prenume_mama << " " << "Prenume tata: " << d[j].prenume_tata << endl;
+	}
+	return numeFisier;
+}
